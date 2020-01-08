0461803e2f53f434c1d464ddd16e45d7d89b0777
Abisola-Morohunfolu <amorohunfolu@gmail.com>
2020-01-08 13:34:26 +0100

    added about component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Contact from './ContactComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponents';
 import DishDetail from './DishDetails';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
@@ -47,6 +48,7 @@ class Main extends Component {
 					<Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
 					<Route path="/menu/:dishId" component={DishWithId} />
 					<Route exact path="/contactus" component={Contact} />
+					<Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
 					<Redirect to="/home" />
 				</Switch>
 
