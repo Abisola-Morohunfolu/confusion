29ec1c48a99b829aa085f37e8dadbec9e11071e9
Abisola-Morohunfolu <amorohunfolu@gmail.com>
2020-01-14 21:31:03 +0100

    added-redux-form

--- src/components/DishDetails.js
+++ src/components/DishDetails.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
 	Card,
 	CardImg,
@@ -6,59 +6,175 @@ import {
 	CardTitle,
 	CardBody,
 	Breadcrumb,
-	BreadcrumbItem
+	BreadcrumbItem,
+	Button,
+	Modal,
+	ModalBody,
+	ModalHeader,
+	Label,
+	Row,
+	Col
 } from 'reactstrap';
+import { LocalForm, Errors, Control } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 
-const DishDetail = props => {
-	const { dish, comments } = props;
-	const singleComment = comments.map(comment => {
+const required = val => val && val.length;
+const maxLength = len => val => !val || val.length <= len;
+const minLength = len => val => val && val.length >= len;
+
+class DishDetail extends Component {
+	state = {
+		isModalOpen: false
+	};
+
+	toggleModal = () => {
+		const val = this.state.isModalOpen;
+		this.setState({
+			isModalOpen: !val
+		});
+	};
+
+	handleSubmit = values => {
+		console.log(`Current State is ${JSON.stringify(values)}`);
+		alert(`Current State is ${JSON.stringify(values)}`);
+	};
+
+	render() {
+		const { dish, comments } = this.props;
+		const singleComment = comments.map(comment => {
+			return (
+				<div key={comment.date}>
+					<p>{comment.comment}</p>
+					<p>
+						-- {comment.author} ,
+						{new Intl.DateTimeFormat('en-US', {
+							year: 'numeric',
+							month: 'short',
+							day: '2-digit'
+						}).format(new Date(Date.parse(comment.date)))}
+					</p>
+				</div>
+			);
+		});
 		return (
-			<div key={comment.date}>
-				<p>{comment.comment}</p>
-				<p>
-					-- {comment.author} ,
-					{new Intl.DateTimeFormat('en-US', {
-						year: 'numeric',
-						month: 'short',
-						day: '2-digit'
-					}).format(new Date(Date.parse(comment.date)))}
-				</p>
+			<div className="container">
+				<div className="row">
+					<Breadcrumb>
+						<BreadcrumbItem>
+							<Link to="/home">Home</Link>
+						</BreadcrumbItem>
+						<BreadcrumbItem>
+							<Link to="/menu">Menu</Link>
+						</BreadcrumbItem>
+						<BreadcrumbItem active>{dish.name}</BreadcrumbItem>
+					</Breadcrumb>
+					<div className="col-12">
+						<h3>{dish.name}</h3>
+						<hr />
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-12 col-md-5 m-1">
+						<Card>
+							<CardImg width="100%" src={dish.image} alt={dish.name} />
+							<CardBody>
+								<CardTitle>{dish.name}</CardTitle>
+								<CardText>{dish.description}</CardText>
+							</CardBody>
+						</Card>
+					</div>
+					<div className="col-12 col-md-5 m-1 Comment">
+						{singleComment}
+
+						<Button color="outline" onClick={this.toggleModal}>
+							<span className="fa fa-pencil fa-lg"></span>Submit A Comment
+						</Button>
+					</div>
+				</div>
+				<Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+					<ModalHeader toggle={this.toggleModal}>Submit a Comment</ModalHeader>
+					<ModalBody>
+						<LocalForm onSubmit={values => this.handleSubmit(values)}>
+							<Row className="form-group">
+								<Label htmlFor="rating" md={2}>
+									Ratings
+								</Label>
+								<Col md={12}>
+									<Control.select model=".rating" name="rating" className="form-control">
+										<option>1</option>
+										<option>2</option>
+										<option>3</option>
+										<option>4</option>
+										<option>5</option>
+									</Control.select>
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Label htmlFor="author" md={3}>
+									Your Name
+								</Label>
+								<Col md={12}>
+									<Control.text
+										model=".author"
+										name="author"
+										id="author"
+										placeholder="Your Name"
+										className="form-control"
+										validators={{
+											required,
+											minLength: minLength(3),
+											maxLength: maxLength(15)
+										}}
+									/>
+									<Errors
+										className="text-danger"
+										model=".author"
+										show="touched"
+										messages={{
+											required: 'This field is required',
+											minLength: 'Must be more than 2 characters',
+											maxLength: 'Must be 15 characters or less'
+										}}
+									/>
+								</Col>
+							</Row>
+							<Row className="form-group">
+								<Label htmlFor="comment" md={3}>
+									Comment
+								</Label>
+								<Col md={12}>
+									<Control.textarea
+										model=".comment"
+										name="comment"
+										id="comment"
+										rows="6"
+										inputMode="text"
+										className="form-control"
+										validators={{
+											required,
+											minLength: minLength(3)
+										}}
+									/>
+									<Errors
+										className="text-danger"
+										model=".comment"
+										show="touched"
+										messages={{
+											required: 'This field is required',
+											minLength: 'Must be more than 2 characters'
+										}}
+									/>
+								</Col>
+							</Row>
+							<Button type="submit" value="submit" color="primary">
+								Submit
+							</Button>
+						</LocalForm>
+					</ModalBody>
+				</Modal>
 			</div>
 		);
-	});
-
-	return (
-		<div className="container">
-			<div className="row">
-				<Breadcrumb>
-					<BreadcrumbItem>
-						<Link to="/home">Home</Link>
-					</BreadcrumbItem>
-					<BreadcrumbItem>
-						<Link to="/menu">Menu</Link>
-					</BreadcrumbItem>
-					<BreadcrumbItem active>{dish.name}</BreadcrumbItem>
-				</Breadcrumb>
-				<div className="col-12">
-					<h3>{dish.name}</h3>
-					<hr />
-				</div>
-			</div>
-			<div className="row">
-				<div className="col-12 col-md-5 m-1">
-					<Card>
-						<CardImg width="100%" src={dish.image} alt={dish.name} />
-						<CardBody>
-							<CardTitle>{dish.name}</CardTitle>
-							<CardText>{dish.description}</CardText>
-						</CardBody>
-					</Card>
-				</div>
-				<div className="col-12 col-md-5 m-1 Comment">{singleComment}</div>
-			</div>
-		</div>
-	);
-};
+	}
+}
 
 export default DishDetail;
